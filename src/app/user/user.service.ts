de71dfb9752a7bc81f5f7a85b348b98f82ccdb62
Michalis Fournarakis <mixfou@gmail.com>
2023-04-13 23:40:12 +0300

    refactored user ui

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -3,17 +3,30 @@ import { Injectable } from '@angular/core';
 import { User, UserAPIList } from './user.interfaces';
 import { delay } from 'rxjs';
 
-const USER_API = 'https://codingfactory.ddns.net/api/user';
+const BASE_URL = 'https://codingfactory.ddns.net';
+const USER_API = BASE_URL + '/api/user';
 
 @Injectable()
 export class UserService {
   constructor(private http: HttpClient) {}
 
   findAll() {
-    return this.http.get<UserAPIList>(`${USER_API}/findall`).pipe(delay(1000));
+    return this.http.get<UserAPIList>(`${USER_API}/findAll`).pipe(delay(500));
+  }
+
+  findByUsername(username: string) {
+    return this.http.get<User>(`${USER_API}/findOne/${username}`);
   }
 
   insertUser(user: User) {
-    return this.http.post<UserAPIList>(`${USER_API}/create`, user);
+    return this.http.post<User>(`${USER_API}/create`, user);
+  }
+
+  updateUser(user: User) {
+    return this.http.patch<User>(`${USER_API}/update`, user);
+  }
+
+  deleteUser(username: string) {
+    return this.http.delete<User>(`${USER_API}/delete/${username}`);
   }
 }
